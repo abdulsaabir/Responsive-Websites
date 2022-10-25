c1c5d0d6d42d219dee94fe1e8bd023ddba2d2fac
Eng-Abdulsaabir-Haaji <saabirhaaji10@gmail.com>
2022-10-25 14:28:12 +0300

    done

--- 03-Hulu Clone/tailwind.config.js
+++ 03-Hulu Clone/tailwind.config.js
@@ -6,35 +6,3 @@ module.exports = {
   },
   plugins: [],
 };
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Stream TV and Movies Live and Online | Hulu</title>
-  </head>
-  <body bg-red-700>
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <button>Log In</button>
-          </li>
-        </ul>
-      </nav>
-
-      <div>
-        <h4>Try up to one month free</h4>
-        <img src="../img/logo.png" alt="Hulu" />
-        <div>Watch thousands of TV shows and movies.</div>
-        <div>
-          HBO Max™, SHOWTIME®, CINEMAX® and STARZ® available as add-ons.
-        </div>
-        <button>Start Your Free Trial</button>
-        <div>Free trial for new & eligible returning subscribers only.</div>
-      </div>
-    </header>
-  </body>
-</html>
