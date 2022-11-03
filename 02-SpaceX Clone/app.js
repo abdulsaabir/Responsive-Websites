6670a802df29052562fc4cb6361cff951a75b603
Abdulsaabir <saabirhaaji10@gmail.com>
2022-11-03 23:24:02 +0300

    spaceX

--- 02-SpaceX Clone/app.js
+++ 02-SpaceX Clone/app.js
@@ -1,7 +1,9 @@
 let btnMenu = document.getElementById('btn-menu')
-
+let overLay = document.getElementById('overLay')
 btnMenu.addEventListener('click' , MenuShow)
 
 function MenuShow(){
     btnMenu.classList.toggle('open')
+    overLay.classList.toggle('show-overlay')
+    document.body.classList.toggle('stop-scrolling')
 }
