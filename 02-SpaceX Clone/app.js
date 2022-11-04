05979cc81502f384298c7b6c2e80d6ca0573337b
Abdulsaabir <saabirhaaji10@gmail.com>
2022-11-04 19:08:51 +0300

    Simon Game

--- 02-SpaceX Clone/app.js
+++ 02-SpaceX Clone/app.js
@@ -1,11 +1,14 @@
 let btnMenu = document.getElementById('btn-menu')
 let overLay = document.getElementById('overLay')
-let showMenu = document.querySelector('.mobile-main-menu')
+let showMenu = document.querySelector('.mobile-main-menu');
+let counter = document.querySelectorAll('.counter')
 btnMenu.addEventListener('click' , MenuShow)
 
 function MenuShow(){
     btnMenu.classList.toggle('open')
     overLay.classList.toggle('show-overlay')
     document.body.classList.toggle('stop-scrolling')
-    showMenu.classList.toggle('show-menu')
+    showMenu.classList.toggle('show-menu    ')
 }
+
+counter.forEach(count => )
