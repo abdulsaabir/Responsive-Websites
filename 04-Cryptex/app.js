a564eb2da25d4f8e9f20e60535da91a9f542fda8
Abdulsaabir <saabirhaaji10@gmail.com>
2022-11-05 21:06:45 +0300

    done

---
+++ 04-Cryptex/app.js
@@ -0,0 +1,9 @@
+let ulNav = document.querySelector('.ulNav')
+let menu = document.querySelector('.menu')
+// dfdsaf
+menu.addEventListener('click', openModal)
+
+function openModal (){
+    console.log(54)
+ulNav.classList.toggle('show')
+}
