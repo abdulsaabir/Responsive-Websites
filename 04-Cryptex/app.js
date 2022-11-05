ad9bba97e4a707786a3c0ca0fdac72b789657f0a
Abdulsaabir <saabirhaaji10@gmail.com>
2022-11-05 22:30:02 +0300

    done

--- 04-Cryptex/app.js
+++ 04-Cryptex/app.js
@@ -1,9 +1,32 @@
-let ulNav = document.querySelector('.ulNav')
-let menu = document.querySelector('.menu')
-// dfdsaf
-menu.addEventListener('click', openModal)
-
-function openModal (){
-    console.log(54)
-ulNav.classList.toggle('show')
+let bitcon = document.getElementById('bitcon')
+// let anim ;
+function ScrollCheck () {
+    let Scrol = window.scrollY;
+    if(Scrol > 266.6666564941406 )
+    {
+      
+      let  anim= document.getElementById("bitcon").animate(
+            [
+                0% {
+                    opacity: 0,
+                    transform: `translateY(140+px)`,
+                  } ,
+                  100% {
+                      opacity: 1,
+                      transform: `translateY(0+px)`,
+                  }
+            ]
+    )
+      
+    anim.pause();
+    }
 }
+document.addEventListener('scroll' , ScrollCheck)
+// const anim = document.getElementById("foo").animate(
+//     [
+//       { transform: `rotate(${A_DYNAMIC_VALUE})` }
+//     ],
+//     { duration: 3000, iterations: Infinity }
+//   );
+  
+  // and later
