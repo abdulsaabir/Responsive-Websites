25dc4a5c90466f118ad96e142126e0c769655462
Abdulsaabir <saabirhaaji10@gmail.com>
2022-11-06 19:00:38 +0300

    done

--- 04-Cryptex/app.js
+++ 04-Cryptex/app.js
@@ -4,29 +4,6 @@ function ScrollCheck () {
     let Scrol = window.scrollY;
     if(Scrol > 266.6666564941406 )
     {
-      
-      let  anim= document.getElementById("bitcon").animate(
-            [
-                0% {
-                    opacity: 0,
-                    transform: `translateY(140+px)`,
-                  } ,
-                  100% {
-                      opacity: 1,
-                      transform: `translateY(0+px)`,
-                  }
-            ]
-    )
-      
-    anim.pause();
-    }
+      }
 }
 document.addEventListener('scroll' , ScrollCheck)
-// const anim = document.getElementById("foo").animate(
-//     [
-//       { transform: `rotate(${A_DYNAMIC_VALUE})` }
-//     ],
-//     { duration: 3000, iterations: Infinity }
-//   );
-  
-  // and later
