6d527f266658b100b6d2f2b796b7a0d1e6c458d6
abdulsaabir <saabirhaaji10@gmail.com>
2022-11-26 21:25:33 +0300

    working on the last part of the form'

--- 12-Animated Form/app.js
+++ 12-Animated Form/app.js
@@ -1,9 +1,7 @@
 let btnToggle = document.querySelectorAll(".btn-toggle");
 let Main = document.querySelector("main");
-console.log(btnToggle);
 btnToggle.forEach((btn) => {
   btn.addEventListener("click", () => {
-    console.log(5464);
     Main.classList.toggle("sign-up-mode");
   });
 });
