80f2e1b2973059a937084996f3d2f67371ee8389
abdulsaabir <saabirhaaji10@gmail.com>
2022-11-26 19:18:08 +0300

    left

--- 12-Animated Form/app.js
+++ 12-Animated Form/app.js
@@ -2,12 +2,16 @@ let Images = ["./img/image1.png", "./img/image2.png", "./img/image3.png"];
 
 let dots = document.querySelectorAll("nav");
 let image = document.getElementById("image");
+let imageContainer = document.querySelector(".imgConainer");
 dots.forEach((dot) => {
-  dot.addEventListener("click", () => {
+  dot.addEventListener("click", (e) => {
     removeDot();
     dot.classList.add("focused");
     image.src = Images[dot.dataset.num - 1];
-    image.classList.add(".image");
+    imageContainer.classList.remove("image");
+    setTimeout(() => {
+      imageContainer.classList.add("image");
+    }, 2000);
   });
 });
 
