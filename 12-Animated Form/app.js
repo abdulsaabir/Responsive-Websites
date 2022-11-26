e9743698e80551589cb7f94d49f6f723654f15a1
abdulsaabir <saabirhaaji10@gmail.com>
2022-11-26 20:17:11 +0300

    making the slide animation

--- 12-Animated Form/app.js
+++ 12-Animated Form/app.js
@@ -1,22 +1,9 @@
-let Images = ["./img/image1.png", "./img/image2.png", "./img/image3.png"];
-
-let dots = document.querySelectorAll("nav");
-let image = document.getElementById("image");
-let imageContainer = document.querySelector(".imgConainer");
-dots.forEach((dot) => {
-  dot.addEventListener("click", (e) => {
-    removeDot();
-    dot.classList.add("focused");
-    image.src = Images[dot.dataset.num - 1];
-    imageContainer.classList.remove("image");
-    setTimeout(() => {
-      imageContainer.classList.add("image");
-    }, 2000);
+let btnToggle = document.querySelectorAll(".btn-toggle");
+let Main = document.querySelector("main");
+console.log(btnToggle);
+btnToggle.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log(5464);
+    Main.classList.toggle("sign-up-mode");
   });
 });
-
-const removeDot = () => {
-  dots.forEach((dot) => {
-    dot.classList.remove("focused");
-  });
-};
