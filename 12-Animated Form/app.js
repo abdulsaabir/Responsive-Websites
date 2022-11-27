259102ebf2d38e77090f53e05f595cc79e54a02e
abdulsaabir <saabirhaaji10@gmail.com>
2022-11-27 16:54:40 +0300

    setting the background image'

--- 12-Animated Form/app.js
+++ 12-Animated Form/app.js
@@ -7,9 +7,11 @@ btnToggle.forEach((btn) => {
 });
 
 let bullets = document.querySelectorAll(".bullets span");
+let imagesWrapper = document.querySelectorAll(".images-wrapper .image");
 bullets.forEach((bullet) => {
   bullet.addEventListener("click", (e) => {
     let value = bullet.dataset.value;
+    ImagesActive(value);
     RemoveActive();
     bullet.classList.add("active");
   });
@@ -20,3 +22,25 @@ function RemoveActive() {
     bullet.classList.remove("active");
   });
 }
+function ImagesActive(value) {
+  imagesWrapper.forEach((image) => {
+    if (image.classList.contains(`img-${value}`)) {
+      image.classList.add("show");
+    } else {
+      image.classList.remove("show");
+    }
+  });
+}
+
+// 1- Closures
+// 2- prototypes
+// 3- es-6
+// part two
+// 4- async/ await
+// 5-event loop
+// 6 -micro tasks / promises / callback
+// 7-v8-engine
+// 8-debugging & profiling
+// 9 - ecoSystem => ex  web-back / babel
+//
+//
