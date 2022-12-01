08ac5b6abdada1d978dd7c6755e38c4db43ed25d
abdulsaabir <saabirhaaji10@gmail.com>
2022-12-01 19:05:29 +0300

    setting up the second section of ul

--- 14-Dashboard Sidebar/app.js
+++ 14-Dashboard Sidebar/app.js
@@ -2,14 +2,17 @@ let btns = document.querySelector("nav i");
 let header = document.querySelector("header");
 let cond = true;
 let li = document.querySelectorAll("li");
+let section = document.querySelector("section");
 btns.addEventListener("click", () => {
   if (cond) {
     btns.classList.remove("fa-greater-than");
     btns.classList.add("fa-less-than");
+    section.style.marginLeft = "16rem";
     cond = false;
   } else {
     btns.classList.remove("fa-less-than");
     btns.classList.add("fa-greater-than");
+    section.style.marginLeft = "7rem";
     cond = true;
   }
   header.classList.toggle("active");
