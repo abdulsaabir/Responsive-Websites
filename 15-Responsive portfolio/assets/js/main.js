3d5d627d9dcf4bd5dd72a7e31417b8a8f91e252d
Abdulsaabir <saabirhaaji10@gmail.com>
2022-12-17 17:26:03 +0300

    modal toggle is working

--- 15-Responsive portfolio/assets/js/main.js
+++ 15-Responsive portfolio/assets/js/main.js
@@ -13,3 +13,19 @@ function ScrollHeader() {
    this.scrollY > 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll' , ScrollHeader)
+
+// modal toggle 
+
+let service_Button = document.querySelectorAll('.service_Button') 
+
+service_Button.forEach(btn => {
+   btn.addEventListener('click' , (e) =>  {
+      let ParentElement = e.target.parentElement;
+      let modal = ParentElement.querySelector('.service_modal');
+      modal.classList.add('active_modal')
+      let ModalClose = ParentElement.querySelector('.service_modal_close')
+      ModalClose.addEventListener('click',() => {
+         modal.classList.remove('active_modal')
+      })
+   })
+})
